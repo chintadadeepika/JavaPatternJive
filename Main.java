a2526be64e12c78c8dbc2fd47551759ca733ee46
chintadadeepika <131290943+chintadadeepika@users.noreply.github.com>
2024-03-11 12:34:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main{
+    static void pattern(int N)
+    {
+        for (int i = 0; i < N ; i++)
+        {
+            
+            for (int j = 0; j <=i; j++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+       public static void main(String[] args) {
+        int N= 5;
+        pattern(N);
+         }
+}
